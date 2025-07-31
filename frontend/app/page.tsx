2037b3f6c0a95f8911878d5f16ac61a982563b17
unknown <christianlugod05@gmail.com>
2025-07-31 18:06:19 +0800

    added fetch on homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,20 +4,43 @@ import Link from "next/link";
 import { dummyEvents } from "@/data/events";
 import EventCard from "@/components/EventCard";
 import { Button } from "@/components/ui/button";
+import useApi from "@/hooks/useApi";
+import { useEffect, useState } from "react";
+import { Event } from "@/types/event";
+import { useAuthContext } from "@/contexts/AuthContext";
 
 export default function HomePage() {
+  const { isLoggedIn, username } = useAuthContext();
+  const [events, setEvents] = useState<Event[]>([]);
+  const { getEvents } = useApi();
+
+  const fetchEvents = async () => {
+    const events = await getEvents();
+    setEvents(events);
+  };
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-50 py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center mb-8">
           <h1 className="text-3xl font-bold">Public Events</h1>
-          <Button variant="outline" asChild>
-            <Link href="/login">Log In</Link>
-          </Button>
+          {!isLoggedIn ? (
+            <Button variant="outline" asChild>
+              <Link href="/login">Log In</Link>
+            </Button>
+          ) : (
+            <Button variant="outline" asChild>
+              <Link href="/dashboard">Dashboard</Link>
+            </Button>
+          )}
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-          {dummyEvents.map((event) => (
+          {events.map((event) => (
             <EventCard key={event.id} event={event} isPublic={true} />
           ))}
         </div>
